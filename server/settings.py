1973836d2ce59dc592aeb12536381eb605141d97
stroici-ion <stroici.ion.22@gmail.com>
2024-09-08 21:39:47 +0300

    Deployment 0.7

--- server/settings.py
+++ server/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-54_z$60zmz(e=rnu-ow(47ajvmghkzfbf_%cwe(10+_&pv@=hk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://13.49.120.104:8000']
+ALLOWED_HOSTS = ['13.49.120.104']
 
 
 # Application definition
