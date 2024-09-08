dfb48898959f9f6570bc8c716c476e165a353ada
stroici-ion <stroici.ion.22@gmail.com>
2024-09-08 16:19:22 +0300

    Deployment 0.6

--- server/settings.py
+++ server/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-54_z$60zmz(e=rnu-ow(47ajvmghkzfbf_%cwe(10+_&pv@=hk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://5qg0riqhvg.execute-api.us-west-2.amazonaws.com/']
+ALLOWED_HOSTS = ['https://5qg0riqhvg.execute-api.us-west-2.amazonaws.com']
 
 
 # Application definition
@@ -159,4 +159,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = ['https://main.d30x8effksuz01.amplifyapp.com/']
+CORS_ALLOWED_ORIGINS = ['https://main.d30x8effksuz01.amplifyapp.com']
