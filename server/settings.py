96b02b930049c433e724db83bed7f406a56347b3
stroici-ion <stroici.ion.22@gmail.com>
2024-09-16 09:33:29 +0300

    Commit

--- server/settings.py
+++ server/settings.py
@@ -159,9 +159,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = ['https://main.du44a9x9lupkf.amplifyapp.com']
+# CORS_ALLOWED_ORIGINS = ['https://main.du44a9x9lupkf.amplifyapp.com']
 # CORS_ALLOW_ALL_ORIGIN = True
-# CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 CORS_ALLOW_METHODS = [
     'GET',
